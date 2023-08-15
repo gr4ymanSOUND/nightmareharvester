7f4085e5ae1115371f9e8f9689f66ab8f602efcc
Austin Lawrence <austin.lawrence.al@gmail.com>
2023-08-14 19:18:42 -0500

    Set up basic react app folder structure and installed npm modules

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './components/App';
+
+import './style/nightmare-style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
